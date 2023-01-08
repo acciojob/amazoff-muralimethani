46f3d6a75d5a13c68c94305598a8166f9627cf87
Murali <m.muralibtech496@gmail.com>
2023-01-08 12:20:26 +0530

    amazon

--- src/main/java/com/driver/DeliveryPartner.java
+++ src/main/java/com/driver/DeliveryPartner.java
@@ -13,6 +13,9 @@ public class DeliveryPartner {
     public String getId() {
         return id;
     }
+    public String setId() {
+        return id;
+    }
 
     public Integer getNumberOfOrders(){
         return numberOfOrders;
--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -5,15 +5,35 @@ public class Order {
     private String id;
     private int deliveryTime;
 
-    public Order(String id, String deliveryTime) {
+    public Order(String id, int deliveryTime) {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+
+        this.id =id;
+        this.deliveryTime=deliveryTime;
     }
+
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public int getDeliveryTime() {return deliveryTime;}
+
 
     public String getId() {
         return id;
     }
 
-    public int getDeliveryTime() {return deliveryTime;}
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setDeliveryTime(int deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
 }
